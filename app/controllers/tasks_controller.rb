7df77ba724d8fef34420a5717c366fa5e220bc4f
Kimon Haars <kimon.haars@gmail.com>
2021-01-09 16:04:55 +0100

    US 3, adding a new task, is implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,20 @@ class TasksController < ApplicationController
     # raise
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
